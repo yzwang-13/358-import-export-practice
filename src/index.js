2ddd47c777c4a24bd642b0c4c7ee11f86daa00ac
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-10 18:56:07 +1100

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { add, multiply, subtract, divide } from "./calculator";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
